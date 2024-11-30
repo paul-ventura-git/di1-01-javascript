83a63e5abcbe473cfa944a3f34eeeab08cf7c35f
paul-ventura-git <j.paul.ventura@gmail.com>
2024-11-30 18:44:55 -0500

    Update 03destructuring.js
    adding something to destructuring...

---
+++ 16objects/03destructuring.js
@@ -0,0 +1,34 @@
+const vehicleOne = {
+  brand: 'Ford',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021, 
+  color: 'red'
+}
+
+myVehicle(vehicleOne);
+
+function myVehicle({type, color, brand, model}) {
+const message = 'My ' + type + ' is a ' + color + ' ' + brand + ' ' + model + '.';
+console.log(message);
+}
+
+const vehicleTwo = {
+brand: 'Ford',
+model: 'Mustang',
+type: 'car',
+year: 2021, 
+color: 'red',
+  registration: {
+    city: 'Houston',
+    state: 'Texas',
+    country: 'USA'
+  }
+}
+
+myVehicleTwo(vehicleTwo)
+
+function myVehicleTwo({ model, registration: { state } }) {
+const message = 'My ' + model + ' is registered in ' + state + '.';
+console.log(message);
+}
