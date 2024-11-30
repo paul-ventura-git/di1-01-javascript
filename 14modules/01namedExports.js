c0dde1f03cf67edee8ddb8be012a32de2517e116
paul-ventura-git <j.paul.ventura@gmail.com>
2024-11-30 18:27:55 -0500

    default exports

--- 14modules/01namedExports.js
+++ 14modules/01namedExports.js
@@ -2,3 +2,8 @@
 //    <script type="module" ...
 // para que el navegador pueda entender que se trata de un archivo de módulo
 
+import { name } from '../assets/js/14namedExports.js'
+import { age } from '../assets/js/14namedExports.js'
+
+console.log(name);
+console.log(age);
---
+++ 14modules/02defaultExports.js
@@ -0,0 +1,6 @@
+// Los "default export" no necesitan llaves {} al importarse.
+
+import message from "../assets/js/14defaultExports.js"
+
+console.log(message);
+console.log(message());
